89506ca43f1c83205b40999497635b7f730f344f
Plajdo <31418257+Plajdo@users.noreply.github.com>
2017-10-21 21:31:15 +0200

    GUI WORKS
    YESSSSS

--- src/es/esy/playdotv/Load.java
+++ src/es/esy/playdotv/Load.java
@@ -3,6 +3,7 @@ package es.esy.playdotv;
 import javax.swing.UIManager;
 
 import de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel;
+import de.javasoft.plaf.synthetica.SyntheticaRootPaneUI;
 import es.esy.playdotv.gui.MainMenu;
 
 public class Load {
@@ -10,11 +11,16 @@ public class Load {
 	public static void main(String[] args){
 		try{
 			UIManager.setLookAndFeel(new SyntheticaAluOxideLookAndFeel());
+			System.out.println(SyntheticaRootPaneUI.EVAL_COPY);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		MainMenu.open();
 		
+	}
+	
+	public static final boolean isEvalCopy(){
+		return false;
 	}
 
 }
