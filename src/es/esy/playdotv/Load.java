4813d145f4ca8ab1253d9c8e8d0156c6790be7d0
Plasmoxy <sebohacker@gmail.com>
2017-10-21 17:43:15 +0200

    material ui

--- src/es/esy/playdotv/Load.java
+++ src/es/esy/playdotv/Load.java
@@ -1,18 +1,25 @@
 package es.esy.playdotv;
 
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
+import atarw.materiallook.GUITheme;
+import atarw.materiallook.MaterialLookAndFeel;
 import es.esy.playdotv.gui.MainMenu;
 
 public class Load {
 
 	public static void main(String[] args)
 	{
-		/*
-		try {UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}
-		catch (Exception e) {e.printStackTrace();}
-		*/ // looks like shit
-		
+		MaterialLookAndFeel ui = new MaterialLookAndFeel (GUITheme.LIGHT_THEME);
+	    
+		try {
+			UIManager.setLookAndFeel (ui.getName ());
+		}
+		catch (UnsupportedLookAndFeelException e) {}
+		catch (ClassNotFoundException e) {}
+		catch (InstantiationException e) {}
+		catch (IllegalAccessException e) {}
 		
 		
 		MainMenu.open();
