a36abff8a5b15d6da22300639b9c4a1bad818d21
Plasmoxy <sebohacker@gmail.com>
2017-10-22 12:12:25 +0200

    remap

--- src/es/esy/playdotv/objects/Paper.java
+++ src/es/esy/playdotv/objects/Paper.java
@@ -4,14 +4,14 @@ import java.io.Serializable;
 import java.util.Date;
 
 public interface Paper extends Serializable {
-	public String getID();
-	public String getTitle();
-	public String getAuthor();
-	public boolean isBorrowed();
-	public void setTitle(String title);
-	public void setAuthor(String author);
-	public void setBorrowed(boolean borrowed);
-	public void setBorrowDate(int day, int month, int year, int untilDay, int untilMonth, int untilYear);
-	public Date getBorrowedDate();
-	public Date getBorrowedUntilDate();
+	String getID();
+	String getTitle();
+	String getAuthor();
+	boolean isBorrowed();
+	void setTitle(String title);
+	void setAuthor(String author);
+	void setBorrowed(boolean borrowed);
+	void setBorrowDate(int day, int month, int year, int untilDay, int untilMonth, int untilYear);
+	Date getBorrowedDate();
+	Date getBorrowedUntilDate();
 }
