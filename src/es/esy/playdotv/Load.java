8c726a053595b9594fbb763f53c45ca1ecb4a3de
Plasmoxy <sebohacker@gmail.com>
2017-10-22 13:48:31 +0200

    update

--- src/es/esy/playdotv/Load.java
+++ src/es/esy/playdotv/Load.java
@@ -1,16 +1,9 @@
 package es.esy.playdotv;
 
-import javax.swing.UIManager;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
-
-import com.jtattoo.plaf.hifi.HiFiLookAndFeel;
-import com.jtattoo.plaf.noire.NoireLookAndFeel;
-import com.sun.java.swing.plaf.motif.MotifLookAndFeel;
-import com.jtattoo.plaf.aluminium.AluminiumLookAndFeel;
 import com.jtattoo.plaf.graphite.GraphiteLookAndFeel;
-
 import es.esy.playdotv.gui.MainMenu;
+
+import javax.swing.*;
 
 public class Load {
 
