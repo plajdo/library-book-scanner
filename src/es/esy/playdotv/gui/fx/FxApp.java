0d9b73204c8a1b615c14de31ff7a17224b3e2222
Plajdo <31418257+Plajdo@users.noreply.github.com>
2017-12-02 00:41:33 +0100

    FX rework #3.1
    Fixed imports.

--- src/es/esy/playdotv/gui/fx/FxApp.java
+++ src/es/esy/playdotv/gui/fx/FxApp.java
@@ -1,7 +1,5 @@
 package es.esy.playdotv.gui.fx;
 
-import com.gluonhq.charm.glisten.visual.Theme;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
