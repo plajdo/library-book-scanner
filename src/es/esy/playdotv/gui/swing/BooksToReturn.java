63983d14a3994c6835e77dde12dc769f34be57d1
Plajdo <31418257+Plajdo@users.noreply.github.com>
2017-12-12 21:47:11 +0100

    BooksToReturn work
    Finally v2

--- src/es/esy/playdotv/gui/swing/BooksToReturn.java
+++ src/es/esy/playdotv/gui/swing/BooksToReturn.java
@@ -1,16 +1,26 @@
 package es.esy.playdotv.gui.swing;
 
 import com.unaux.plasmoxy.libscan.database.LBSDatabase;
+
+import es.esy.playdotv.event.TableRefreshEvent;
+import es.esy.playdotv.event.TableRefreshEventListener;
 import es.esy.playdotv.objects.Book;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.util.Date;
 
 public class BooksToReturn extends JInternalFrame{
 	
 	private static final long serialVersionUID = 1L;
 	private JTable table;
+	private JScrollPane scrollPane;
+	private DefaultTableModel tblModel;
 	
 	private LBSDatabase db = LBSDatabase.getInstance();
 	
@@ -23,17 +33,57 @@ public class BooksToReturn extends JInternalFrame{
 		setBounds(100, 100, 450, 300);
 		
 		table = new JTable();
-		getContentPane().add(new JScrollPane(table));
+		scrollPane = new JScrollPane(table);
 		
-		DefaultTableModel tblModel = new DefaultTableModel(null, new String[]{
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setPreferredSize(new Dimension(getWidth(), getHeight() - 30));
+		
+		getContentPane().add(scrollPane, BorderLayout.NORTH);
+		
+		tblModel = new DefaultTableModel(null, new String[]{
 				"ID knihy", "N\u00E1zov knihy", "Autor knihy", "Vr\u00E1ti\u0165 do"
 		});
 		
 		table.setModel(tblModel);
 		table.getColumnModel().getColumn(3).setCellRenderer(new CellRenderer());
 		
+		AddBook.addDataDialogListener(new TableRefreshEventListener(){
+
+			@Override
+			public void handleTableRefreshEvent(TableRefreshEvent evt) {
+				refreshTable();
+				
+			}
+			
+		});
+		this.addComponentListener(new ComponentListener(){
+			
+			@Override
+			public void componentResized(ComponentEvent e){
+				resizeTable();
+			}
+
+			@Override
+			public void componentHidden(ComponentEvent arg0) {
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent arg0) {
+			}
+
+			@Override
+			public void componentShown(ComponentEvent arg0) {
+			}
+		});
+		
+		refreshTable();
+		
 		setVisible(true);
 
+	}
+	
+	private void addStuffToTable(){
 		for (String key : db.books.keySet()){
 			Book b = db.books.get(key);
 			
@@ -42,7 +92,21 @@ public class BooksToReturn extends JInternalFrame{
 			}
 			
 		}
-
+		
+	}
+	
+	private void clearStuffFromTable(){
+		tblModel.getDataVector().removeAllElements();
+		tblModel.fireTableDataChanged();
+	}
+	
+	private void refreshTable(){
+		clearStuffFromTable();
+		addStuffToTable();
+	}
+	
+	private void resizeTable(){
+		scrollPane.setPreferredSize(getContentPane().getSize());
 	}
 
 }
