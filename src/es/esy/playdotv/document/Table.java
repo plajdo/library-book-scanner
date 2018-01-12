d95eba8a0c5c378b6b6cccf5348944314ef34fd2
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-01-12 13:58:50 +0100

    Gitignore
    No .xls documents on git for now

--- src/es/esy/playdotv/document/Table.java
+++ src/es/esy/playdotv/document/Table.java
@@ -2,25 +2,20 @@ package es.esy.playdotv.document;
 
 import java.io.File;
 
-import es.esy.playdotv.gui.terminal.TermUtils;
-import jxl.Cell;
-import jxl.Sheet;
 import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 public class Table {
 	
 	public static void createTable() throws Exception{
 		
-		Workbook workbook = Workbook.getWorkbook(new File("wb.xls"));
-		Sheet sheet = workbook.getSheet(0);
+		WritableWorkbook workbook = Workbook.createWorkbook(new File("output.xls"));
+		WritableSheet sheet = workbook.createSheet("Zoznam", 0);
 		
-		Cell A2 = sheet.getCell(0, 1);
-		Cell B8 = sheet.getCell(1, 7);
-		
-		TermUtils.println(A2.getContents());
-		TermUtils.println(B8.getContents());
-		
-		workbook.close();
+		Label label = new Label(0, 0, "TRIEDA: KVINTA");
+		sheet.addCell(label);
 		
 	}
 	
