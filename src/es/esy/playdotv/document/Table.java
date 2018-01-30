8af14b3da40e9ed4b1205f119743524d990887ca
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-01-30 10:46:44 +0100

    Export

--- src/es/esy/playdotv/document/Table.java
+++ src/es/esy/playdotv/document/Table.java
@@ -14,13 +14,16 @@ import jxl.write.WritableCellFormat;
 import jxl.write.WritableFont;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
 
-public class Table {
+public class Table{
 	
-	public static void createTable(List<BorrowingEntry> entries, String group) throws Exception{
+	private static int row = 2;
+	
+	public static void createTable(List<BorrowingEntry> entries, String group, String folder) throws Exception{
 		
 		Workbook originalWorkbook = Workbook.getWorkbook(new File("zoznam.xls"));
-		WritableWorkbook workbook = Workbook.createWorkbook(new File("output_" + group + ".xls"), originalWorkbook);
+		WritableWorkbook workbook = Workbook.createWorkbook(new File(folder + "output_" + group + ".xls"), originalWorkbook);
 		WritableSheet sheet = workbook.getSheet(0);
 		
 		Cell A1 = sheet.getCell(0, 0);
@@ -38,8 +41,6 @@ public class Table {
 		Label label = new Label(0, 0, "Evidencia absen\u010Dn\u00FDch v\u00FDpo\u017Ei\u010Diek: " + group, cellFormat);
 		sheet.addCell(label);
 		
-		int cellRow = 2;
-		
 		WritableCellFormat normalCellFormat = new WritableCellFormat();
 		cellFormat.setFont(new WritableFont(WritableFont.createFont("Avenir Next"), 12, WritableFont.NO_BOLD));
 		
@@ -50,24 +51,43 @@ public class Table {
 		normalCellFormat.setVerticalAlignment(v_align_normal);
 		
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+		entries.forEach((entry) -> {
+			Label cellEntryA = new Label(0, getRow(), dateFormat.format(entry.getBorrowDate()));
+			Label cellEntryB = new Label(1, getRow(), entry.getUsername());
+			Label cellEntryC = new Label(2, getRow(), entry.getBookname());
+			Label cellEntryD = new Label(3, getRow(), entry.getBookID());
+			Label cellEntryE = new Label(4, getRow(), (entry.getReturnDate() == null ? "" : dateFormat.format(entry.getReturnDate())));
+
+			try {
+				sheet.addCell(cellEntryA);
+				sheet.addCell(cellEntryB);
+				sheet.addCell(cellEntryC);
+				sheet.addCell(cellEntryD);
+				sheet.addCell(cellEntryE);
+			} catch (WriteException e) {
+				e.printStackTrace();
+			}
+			increaseRow();
+		});
 		
-		for(BorrowingEntry entry : entries){
-			Label cellEntryA = new Label(0, cellRow, dateFormat.format(entry.getBorrowDate()));
-			Label cellEntryB = new Label(1, cellRow, entry.getUsername());
-			Label cellEntryC = new Label(2, cellRow, entry.getBookname());
-			Label cellEntryD = new Label(3, cellRow, entry.getBookID());
-			Label cellEntryE = new Label(4, cellRow, (entry.getReturnDate() == null ? "" : dateFormat.format(entry.getReturnDate())));
-			sheet.addCell(cellEntryA);
-			sheet.addCell(cellEntryB);
-			sheet.addCell(cellEntryC);
-			sheet.addCell(cellEntryD);
-			sheet.addCell(cellEntryE);
-			cellRow++;
-		}
+		resetRow();
 		
 		workbook.write();
 		workbook.close();
 		
+	}
+	
+	private static void increaseRow(){
+		row++;
+	}
+	
+	private static int getRow(){
+		return row;
+	}
+	
+	private static void resetRow(){
+		row = 2;
 	}
 	
 }
