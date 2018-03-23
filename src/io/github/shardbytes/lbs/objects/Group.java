b81b12d289fb88b33797258e499f2e8218168b1b
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-03-23 10:32:29 +0100

    Class class
    class class class class class class class class

---
+++ src/io/github/shardbytes/lbs/objects/Group.java
@@ -0,0 +1,44 @@
+package io.github.shardbytes.lbs.objects;
+
+import java.util.HashSet;
+
+public class Group{
+	
+	private String groupName;
+	private int groupSize;
+	private HashSet<Person> peopleInGroup;
+	
+	public String getName(){
+		return groupName;
+	}
+	
+	public int getSize(){
+		return groupSize;
+	}
+	
+	public HashSet<Person> getPersonSet(){
+		return peopleInGroup;
+	}
+	
+	public void setName(String name){
+		this.groupName = name;
+	}
+	
+	public void setPersonSet(HashSet<Person> set){
+		this.peopleInGroup = set;
+	}
+	
+	public void addPerson(Person p){
+		peopleInGroup.add(p);
+		groupSize++;
+	}
+	
+	public void removePerson(Person p){
+		if(peopleInGroup.contains(p)){
+			peopleInGroup.remove(p);
+			groupSize--;
+		}
+		
+	}
+	
+}
