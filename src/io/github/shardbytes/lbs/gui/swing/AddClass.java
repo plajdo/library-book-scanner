c0b5fb39951cbe63e3b5ed2bbf96106fada0b9e7
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-04-03 18:24:17 +0200

    Class database v1
    Now works, but with strings (need to save Groups, damn it)

--- src/io/github/shardbytes/lbs/gui/swing/AddClass.java
+++ src/io/github/shardbytes/lbs/gui/swing/AddClass.java
@@ -1,23 +1,28 @@
 package io.github.shardbytes.lbs.gui.swing;
 
-import javax.swing.JInternalFrame;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-
-import io.github.shardbytes.lbs.objects.Group;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+import io.github.shardbytes.lbs.database.ClassDatabase;
+import io.github.shardbytes.lbs.objects.Group;
+import net.miginfocom.swing.MigLayout;
 
 public class AddClass extends JInternalFrame{
 	
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
+	
+	private ClassDatabase cdb = ClassDatabase.Companion.getInstance();
 	
 	public AddClass() {
 		setTitle("Prida\u0165 triedu");
@@ -39,33 +44,41 @@ public class AddClass extends JInternalFrame{
 		DefaultListModel<String> listModel = new DefaultListModel<String>();
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setModel(listModel);
-		getContentPane().add(list, "cell 1 1,grow");
+		JScrollPane scroll = new JScrollPane(list);
+		getContentPane().add(scroll, "cell 1 1,grow");
 		
 		JButton btnPrida = new JButton("Prida\u0165");
 		btnPrida.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				cdb.getClassList().add(textField.getText());
+				cdb.save();
+				refreshList(listModel, cdb.getClassList());
+				textField.setText("");
 			}
 		});
 		getContentPane().add(btnPrida, "cell 0 2");
 		
-		/*
-		 * TODO: THIS BLYAT
-		 */
-		for(;;){
-			addGroupToList(listModel, null);
-		}
+		addGroupsToList(listModel, cdb.getClassList());
 		
 		setVisible(true);
 
 	}
 	
-	private void addGroupToList(DefaultListModel<String> model, Group group){
-		model.addElement(group.getName());
+	private void addGroupsToList(DefaultListModel<String> model, ArrayList<String/*TODO: Change to group*/> list){
+		list.forEach((string/*Change to group as well*/) -> {
+			Group group = new Group(string);
+			model.addElement(group.getName());
+		});
+		
 	}
 	
 	private void clearList(DefaultListModel<String> model){
 		model.clear();
+	}
+	
+	private void refreshList(DefaultListModel<String> model, ArrayList<String/*CHANGG*/> groupList){
+		clearList(model);
+		addGroupsToList(model, groupList);
 	}
 
 }
