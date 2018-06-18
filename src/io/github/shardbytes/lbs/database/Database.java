862a9ff70dffcdbce5cdbd5ac38d921116d6140c
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-06-18 15:26:39 +0200

    remove temporary files on exit

--- src/io/github/shardbytes/lbs/database/Database.java
+++ src/io/github/shardbytes/lbs/database/Database.java
@@ -5,6 +5,7 @@ import io.github.shardbytes.lbs.gui.terminal.TermUtils;
 import net.lingala.zip4j.exception.ZipException;
 
 import javax.swing.JOptionPane;
+import java.io.File;
 
 public class Database{
 	
@@ -28,6 +29,13 @@ public class Database{
 		}catch(ZipException e){
 			TermUtils.printerr("Cannot save database");
 			JOptionPane.showMessageDialog(null, "Ukladanie zlyhalo:\n" + e.getMessage(), "Chyba", JOptionPane.ERROR_MESSAGE);
+		}
+		
+	}
+	
+	public static void flagTemp(String... temp){
+		for(String f : temp){
+			new File(f).deleteOnExit();
 		}
 		
 	}
