9fdc3d42bba2326743259e10254b09f376411775
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-06-25 11:38:55 +0200

    loop, switch

--- src/io/github/shardbytes/lbs/database/WebDB.java
+++ src/io/github/shardbytes/lbs/database/WebDB.java
@@ -24,29 +24,32 @@ public class WebDB{
 	
 	public void host(int port){
 		try{
-			serverSocket = new ServerSocket(port);
-			
-			Socket clientSocket = serverSocket.accept();
-			toClient = new ObjectOutputStream(clientSocket.getOutputStream());
-			fromClient = new ObjectInputStream(clientSocket.getInputStream());
-			
-			byte requestByte;
-			
-			while(true){
-				requestByte = fromClient.readByte();
+			out: while(true){
+				serverSocket = new ServerSocket(port);
 				
-				/*
-				 * Respond to "ping"
-				 */
-				if(requestByte == 1){
-					toClient.writeByte(1);
-				}
+				Socket clientSocket = serverSocket.accept();
+				toClient = new ObjectOutputStream(clientSocket.getOutputStream());
+				fromClient = new ObjectInputStream(clientSocket.getInputStream());
 				
-				/*
-				 * Book list request
-				 */
-				if(requestByte == 2){
-					toClient.writeObject(LBSDatabase.getInstance().books);
+				byte requestByte;
+				
+				in: while(true){
+					requestByte = fromClient.readByte();
+					
+					switch(requestByte){
+						case 1: //respond to "ping"
+							toClient.writeByte(1);
+							
+						case 2: //book list request
+							toClient.writeObject(LBSDatabase.getInstance().books);
+							
+						case 126: //disconnect client
+							break in;
+							
+						case 127: //stop the server
+							break out;
+					}
+					
 				}
 				
 			}
